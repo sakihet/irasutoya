526a2ad58fce9d1e76da33cb7bdeaed4fa44c829
saki <sakihet@gmail.com>
2017-06-03 18:12:04 +0900

    implement

--- main.go
+++ main.go
@@ -2,16 +2,26 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"log"
-	"net/http"
+	"os"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
 func main() {
-	resp, err := http.Get("http://www.irasutoya.com")
+	query := os.Args[1]
+	doc, err := goquery.NewDocument("http://www.irasutoya.com/search?q=" + query)
 	if err != nil {
 		log.Fatal(err)
 	}
-	body, err := ioutil.ReadAll(resp.Body)
-	fmt.Println(body)
+	doc.Find(".boxim").Each(func(_ int, s *goquery.Selection) {
+		href, _ := s.Find("a").Attr("href")
+		doc, _ := goquery.NewDocument(href)
+		h2 := doc.Find(".title h2").Text()
+		title := strings.Replace(h2, "\n", "", -1)
+		href2, _ := doc.Find(".entry .separator a").Attr("href")
+		fmt.Println(title)
+		fmt.Println(href2)
+	})
 }
